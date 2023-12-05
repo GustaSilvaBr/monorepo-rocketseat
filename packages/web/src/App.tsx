61a7748dba4f57214af4c6b30545f9665566472f
Guga <gustavo.ferreira0821@gmail.com>
2023-12-04 23:12:29 -0300

    :package: install some dependences on web packages

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,14 +1,6 @@
-import React, { useEffect } from 'react'
-import api from '@omnirepo/axios-config'
-
-// import { Container } from './styles';
+import React from 'react'
 
 const App: React.FC = () => {
-  useEffect(() => {
-    api.get('/').then(response => {
-      console.log(response)
-    })
-  }, [])
 
   return <h1>Hello World</h1>
 }
